84aabec3eff41b1507a1850501777ea7e386ce4a
Mengfanliaoyao <bo1010614@gmail.com>
2025-07-03 00:03:08 +0800

    update module includes

--- premake5.lua
+++ premake5.lua
@@ -17,11 +17,12 @@ project "GLFW"
 		"src/input.c",
 		"src/monitor.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+		"src/platform.c",
+		"src/null*"
 	}
 
 	filter "system:windows"
-		buildoptions { "-std=c11", "-lgdi32" }
 		systemversion "latest"
 		staticruntime "On"
 
@@ -30,8 +31,9 @@ project "GLFW"
 			"src/win32_init.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
-			"src/win32_time.c",
+			"src/win32_module.c",
 			"src/win32_thread.c",
+			"src/win32_time.c",
 			"src/win32_window.c",
 			"src/wgl_context.c",
 			"src/egl_context.c",
@@ -44,7 +46,9 @@ project "GLFW"
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 
-	filter { "system:windows", "configurations:Release" }
-	{
-		buildoptions "/MT"
-	}
+--	filter { "system:windows", "configurations:Debug" }
+--		buildoptions "/MTd"
+
+--	filter { "system:windows", "configurations:Release" }
+--		buildoptions "/MT"
+	
